453421dba871d4fe7f6a9f5786e57b42a9b5349a
Bradley Dice <bdice@bradleydice.com>
2022-03-15 15:25:18 -0500

    Add microbench for list unpacking.

---
+++ microbenchmarks/list_unpack.py
@@ -0,0 +1,33 @@
+import os
+import timeit
+from textwrap import dedent
+
+"""Counting an iterator by consuming it."""
+
+setup = """\
+    data = tuple(range(1000))
+"""
+
+tests = {
+    "list_construct": """\
+        list(data)
+    """,
+    "list_unpack": """\
+        [*data]
+    """,
+}
+
+results = {}
+for test_name, test_stmt in tests.items():
+    times = timeit.repeat(
+        setup=dedent(setup).rstrip(),
+        stmt=dedent(test_stmt).rstrip(),
+        repeat=1000,
+        number=1000,
+        globals={},
+    )
+    avg_time = sum(times) / len(times)
+    results[test_name] = avg_time
+
+for test_name, result in sorted(results.items(), key=lambda x: x[1]):
+    print(f"{test_name:>18}: {result:03g}")
