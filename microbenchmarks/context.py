e6309e85f9ec57e946dd9c458160d232c5edae10
Bradley Dice <bdice@bradleydice.com>
2022-04-09 13:29:39 -0500

    Improve naming.

--- microbenchmarks/context.py
+++ microbenchmarks/context.py
@@ -26,12 +26,12 @@ def catch_context():
         pass
 
 
-def with_catch(fn):
+def with_contextlib_contextmanager(fn):
     with catch_context():
         fn()
 
 
-class ManualCatchContext(object):
+class ManualContextManager:
     def __enter__(self):
         pass
 
@@ -39,29 +39,38 @@ class ManualCatchContext(object):
         return True
 
 
-def manual_with_catch(fn):
-    with ManualCatchContext():
+def with_manual_contextmanager(fn):
+    with ManualContextManager():
         fn()
 
 
-preinstantiated_manual_catch_context = ManualCatchContext()
+preinstantiated_manual_contextmanager = ManualContextManager()
 
 
-def manual_with_catch_cache(fn):
-    with preinstantiated_manual_catch_context:
+def with_preinstantiated_manual_contextmanager(fn):
+    with preinstantiated_manual_contextmanager:
         fn()
 
 
-setup = "from __main__ import simple_catch, work_pass, work_fail, with_catch, manual_with_catch, manual_with_catch_cache"
-commands = [
+setup = "from __main__ import simple_catch, work_pass, work_fail, with_contextlib_contextmanager, with_manual_contextmanager, with_preinstantiated_manual_contextmanager"
+tests = [
     "simple_catch(work_pass)",
     "simple_catch(work_fail)",
-    "with_catch(work_pass)",
-    "with_catch(work_fail)",
-    "manual_with_catch(work_pass)",
-    "manual_with_catch(work_fail)",
-    "manual_with_catch_cache(work_pass)",
-    "manual_with_catch_cache(work_fail)",
+    "with_contextlib_contextmanager(work_pass)",
+    "with_contextlib_contextmanager(work_fail)",
+    "with_manual_contextmanager(work_pass)",
+    "with_manual_contextmanager(work_fail)",
+    "with_preinstantiated_manual_contextmanager(work_pass)",
+    "with_preinstantiated_manual_contextmanager(work_fail)",
 ]
-for c in commands:
-    print(c, ": ", timeit.timeit(c, setup))
+
+results = {}
+for test_stmt in tests:
+    times = timeit.repeat(
+        setup=setup, stmt=test_stmt, repeat=100, number=10000, globals={}
+    )
+    avg_time = sum(times) / len(times)
+    results[test_stmt] = avg_time
+
+for test_name, result in sorted(results.items(), key=lambda x: x[1]):
+    print(f"{test_name:>18}: {result:03g}")
